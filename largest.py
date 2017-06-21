c5668c8ec77424cbcebbfaf3a0a5de3f13d95377
Gayathrikaramani <gayathiri1397@gmail.com>
2017-06-21 16:11:44 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,10 @@
+a=int(input("enter the 1st no"))
+b=int(input("enter the 2nd no"))
+c=int(input("enter the 3rd no"))
+if(a>b)and(a>c):
+    print("largest no is"+str(a))
+elif(b>a)and(b>c):
+  print("largest no is"+str(a))
+
+else:
+  print("largest no is"+str(c))
